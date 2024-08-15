6d253ddddc985529eafb5631f3c610eaecf5b4c9
Quy Duong Dang <dqduong2003@gmail.com>
2024-08-16 00:33:34 +1000

    Upload

---
+++ send-email.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = "dqduong2003@gmail.com"; 
+    $subject = "Contact Form Submission from $name";
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    $email_body = "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $email_body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
